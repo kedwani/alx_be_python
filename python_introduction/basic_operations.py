ce71f6e6b19240b522ca0fb162747379e8f4213a
kedwani <you@example.com>
2025-11-09 17:19:58 +0200

    resolve some code error

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,4 +1,4 @@
-number1= 10
+number1 = 10
 number2 = 5
 print ("addition of", number1, "and", number2, "is", number1 + number2)
 print ("subtraction of", number1, "and", number2, "is", number1 - number2)
