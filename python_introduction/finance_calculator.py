9d9b7a3557d36c6eaa9e43fd3c54a68b685d1980
kedwani <you@example.com>
2025-11-09 17:57:44 +0200

    a

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,5 @@
-mi = int (input ("Enter your monthly income: "))
-me = int (input("Enter your total monthly expenses: "))
+mi = float (input ("Enter your monthly income: "))
+me = float (input("Enter your total monthly expenses: "))
 ms = mi - me
 a_s = (ms * 12 + (ms * 12 * 0.05))
 print ("Your monthly savings are $", ms, sep="")
