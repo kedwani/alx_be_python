d54a41fd168465f0412c644e0c42d3c9307b9499
mahmoud nasser <126265154+kedwani@users.noreply.github.com>
2025-12-12 19:25:43 +0200

    update polymorphism_demo.py

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -2,14 +2,14 @@ import math
 class Shape:
     def area(self):
         raise NotImplementedError("no shape have been defined")
-class Rectangle (Shape):
+class Rectangle(Shape):
     def __init__(self, length, width):
         self.length = length
         self.width = width
     def area(self):
         return self.length * self.width
 
-class Circle (Shape):
+class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
     def area(self):
